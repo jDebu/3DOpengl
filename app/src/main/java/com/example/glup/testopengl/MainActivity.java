e6df84e7ba29f137b0b1040eff420e824468b4a7
jdebu <dmaster_beta@hotmail.com>
2015-12-18 17:34:45 -0500

    obtener version opengl

--- app/src/main/java/com/example/glup/testopengl/MainActivity.java
+++ app/src/main/java/com/example/glup/testopengl/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.glup.testopengl;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        version=(TextView)findViewById(R.id.txt_version_opengl);
+        if (hasGLES20()){
+            version.setText("Tiene opengl "+versionGL());
+        }else{
+            version.setText("no tiene opengl");
+        }
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,4 +59,18 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    private boolean hasGLES20() {
+        ActivityManager am = (ActivityManager)
+                getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        Log.e("GLVersion",info.reqGlEsVersion+"");
+        return info.reqGlEsVersion >= 0x20000;
+    }
+    private String versionGL(){
+        ActivityManager am = (ActivityManager)
+                getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        return info.getGlEsVersion();
+    }
+
 }
